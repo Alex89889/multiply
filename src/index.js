3e72b744b9727dc4ffd3d4534f5bbf7a1a00017f
Alex89889 <mihal4encko.sasha@yandex.ru>
2019-10-07 21:46:08 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  var firstArr = first.split('').reverse();
+  var secondArr = second.split('').reverse();
+
+  var stack = [];
+
+  for (var i = 0; i < firstArr.length; i++) {
+    for (var j = 0; j < secondArr.length; j++) {
+      var multip = firstArr[i] * secondArr[j];
+      if(stack[i + j]) 
+		  stack[i + j] = stack[i + j] + multip;
+      else 
+		  stack[i + j] = multip;
+    }
+  }
+
+  for (var i = 0; i < stack.length; i++) {
+    var stackFloor = Math.floor(stack[i] / 10);
+    if (stack[i + 1]) 
+		stack[i + 1] += stackFloor;   
+    else if(stackFloor!= 0) 
+		stack[i + 1] = stackFloor;
+    stack[i] =  stack[i] % 10;
+  }
+  
+  return stack.reverse().join('');
 }
